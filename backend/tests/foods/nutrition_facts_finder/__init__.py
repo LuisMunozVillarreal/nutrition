d96e7927dd6cfabf5383b825404c27dbe610143d
Luis Muñoz <luis.munoz.villarreal@gmail.com>
2024-11-02 02:41:35 +0000

    Get nutrition facts using Gemini

---
+++ backend/tests/foods/nutrition_facts_finder/__init__.py
@@ -0,0 +1,13 @@
+"""nutrition facts finder package."""
+
+DEFAULT_FORM_DATA = {
+    "energy": 0,
+    "protein_g": 0,
+    "fat_g": 0,
+    "carbs_g": 0,
+    "nutritional_info_size": 0,
+    "nutritional_info_unit": "ml",
+    "weight": 0,
+    "weight_unit": "ml",
+    "num_servings": 0,
+}
