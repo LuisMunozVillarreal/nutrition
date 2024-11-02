9bc7c00c652af645ca5cd4526c9197abe410c45b
Luis Muñoz <luis.munoz.villarreal@gmail.com>
2024-11-02 18:00:12 +0000

    Add a few imperial units

--- backend/apps/foods/models/units.py
+++ backend/apps/foods/models/units.py
@@ -1,21 +1,34 @@
 """units module."""
 
+# Weight
+UNIT_KILOGRAM = "kg"
 UNIT_GRAM = "g"
-UNIT_MILIGRAM = "mg"
+UNIT_MILLIGRAM = "mg"
+UNIT_OUNCE = "oz"
+UNIT_POUND = "lb"
+
 UNIT_LITRE = "l"
 UNIT_MILILITRE = "ml"
-UNIT_UNIT = "unit"
 UNIT_CUP = "cup"
+UNIT_TEASPOON = "tsp"
+UNIT_TABLESPOON = "tbsp"
+
+UNIT_UNIT = "unit"
 UNIT_SERVING = "serving"
 UNIT_CONTAINER = "container"
 
 UNIT_CHOICES = (
+    (UNIT_KILOGRAM, "kilogram(s)"),
     (UNIT_GRAM, "gram(s)"),
-    (UNIT_MILIGRAM, "miligram(s)"),
+    (UNIT_MILLIGRAM, "milligram(s)"),
+    (UNIT_OUNCE, "ounce(s)"),
+    (UNIT_POUND, "pound(s)"),
     (UNIT_LITRE, "litre(s)"),
     (UNIT_MILILITRE, "millilitre(s)"),
-    (UNIT_UNIT, "unit(s)"),
+    (UNIT_TEASPOON, "teaspoon(s)"),
+    (UNIT_TABLESPOON, "tablespoon(s)"),
     (UNIT_CUP, "cup(s)"),
+    (UNIT_UNIT, "unit(s)"),
     (UNIT_SERVING, "serving(s)"),
     (UNIT_CONTAINER, "container(s)"),
 )
