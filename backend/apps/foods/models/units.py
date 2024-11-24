89eba33290c38801138f5ca94fb3f5659ec1561b
Luis Muñoz <luis.munoz.villarreal@gmail.com>
2024-11-24 00:32:53 +0000

    Add fluid ounce as unit

--- backend/apps/foods/models/units.py
+++ backend/apps/foods/models/units.py
@@ -1,35 +1,49 @@
 """units module."""
 
 # Weight
-UNIT_KILOGRAM = "kg"
-UNIT_GRAM = "g"
 UNIT_MILLIGRAM = "mg"
+UNIT_GRAM = "g"
+UNIT_KILOGRAM = "kg"
+# - Imperial
 UNIT_OUNCE = "oz"
 UNIT_POUND = "lb"
 
+# Volume
 UNIT_LITRE = "l"
 UNIT_CENTILITRE = "cl"
 UNIT_MILLILITRE = "ml"
-UNIT_CUP = "cup"
+# - Imperial
+UNIT_CUP = "c"
+UNIT_FLUID_OUNCE = "floz"
 UNIT_TEASPOON = "tsp"
 UNIT_TABLESPOON = "tbsp"
 
+# Other
 UNIT_UNIT = "unit"
 UNIT_SERVING = "serving"
 UNIT_CONTAINER = "container"
 
 UNIT_CHOICES = (
-    (UNIT_KILOGRAM, "kilogram(s)"),
-    (UNIT_GRAM, "gram(s)"),
+    #
+    # Weight
     (UNIT_MILLIGRAM, "milligram(s)"),
+    (UNIT_GRAM, "gram(s)"),
+    (UNIT_KILOGRAM, "kilogram(s)"),
+    # - Imperial
     (UNIT_OUNCE, "ounce(s)"),
     (UNIT_POUND, "pound(s)"),
-    (UNIT_LITRE, "litre(s)"),
-    (UNIT_CENTILITRE, "centilitre(s)"),
+    #
+    # Volume
     (UNIT_MILLILITRE, "millilitre(s)"),
-    (UNIT_TEASPOON, "teaspoon(s)"),
-    (UNIT_TABLESPOON, "tablespoon(s)"),
+    (UNIT_CENTILITRE, "centilitre(s)"),
+    (UNIT_LITRE, "litre(s)"),
+    # - Imperial
     (UNIT_CUP, "cup(s)"),
+    (UNIT_FLUID_OUNCE, "fluid ounce(s)"),
+    (UNIT_TABLESPOON, "tablespoon(s)"),
+    (UNIT_TEASPOON, "teaspoon(s)"),
+    #
+    # Other
     (UNIT_UNIT, "unit(s)"),
     (UNIT_SERVING, "serving(s)"),
     (UNIT_CONTAINER, "container(s)"),
