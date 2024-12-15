1e20a8284a3cbcad604b6a0ab3430c30c8b0e48a
Luis Muñoz <luis.munoz.villarreal@gmail.com>
2024-12-15 20:07:36 +0000

    Show error when a serving bigger than available is tried to be consumed

--- backend/tests/plans/test_admin.py
+++ backend/tests/plans/test_admin.py
@@ -4,6 +4,12 @@ import datetime
 
 import pytest
 
+from apps.plans.models import Intake
+
+#
+# Search
+#
+
 
 def test_search_renders_intake(logged_in_admin_client, intake):
     """Admin search renders."""
@@ -30,6 +36,11 @@ def test_search_renders_weekplan(logged_in_admin_client, week_plan):
 
     # Then
     assert result.status_code == 200
+
+
+#
+# Add
+#
 
 
 @pytest.fixture
@@ -78,6 +89,44 @@ def test_add_new_weekplan_renders(logged_in_admin_client):
 
     # Then
     assert result.status_code == 200
+
+
+def test_add_new_intake_bigger_serving_than_available(
+    logged_in_admin_client, cupboard_item, serving, day
+):
+    """Add intake with bigger serving than available is managed."""
+    # Given a request to add an intake with a bigger serving than available
+    response = logged_in_admin_client.post(
+        "/admin/plans/intake/add/",
+        data={
+            "day": day.id,
+            "meal": Intake.MEAL_BREAKFAST,
+            "food": serving.id,
+            "num_servings": 10,
+            "pictures-TOTAL_FORMS": 0,
+            "pictures-INITIAL_FORMS": 0,
+            "pictures-MIN_NUM_FORMS": 0,
+            "pictures-MAX_NUM_FORMS": 1000,
+            "_save": "Save",
+        },
+    )
+
+    # And the response is a redirection
+    assert response.status_code == 302
+
+    # When the user is redirected
+    response = logged_in_admin_client.get("/admin/plans/intake/add/")
+
+    # Then the error message is shown
+    assert (
+        b"Intake can&#x27;t be bigger than the cupboard item&#x27;s quantity."
+        in response.content
+    ), str(response.content)
+
+
+#
+# Change
+#
 
 
 def test_edit_intake_renders(logged_in_admin_client, intake):
