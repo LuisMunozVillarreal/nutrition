d70de04087863aeb799b7f708f75fb18b98c5d31
Luis Muñoz <luis.munoz.villarreal@gmail.com>
2025-04-13 15:07:57 +0100

    Add django-sql-dashboard

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,9 +1,11 @@
 """nutrition URL Configuration."""
 
+import django_sql_dashboard
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("dashboard/", include(django_sql_dashboard.urls)),
     path("_nested_admin/", include("nested_admin.urls")),
 ]
