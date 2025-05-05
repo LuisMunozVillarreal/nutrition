09bdf1fcbb6f335242bff73419438a816c38dd1d
Luis Muñoz <luis.munoz.villarreal@gmail.com>
2025-05-05 22:18:59 +0100

    Energy meets macros gist

--- backend/apps/foods/models/product.py
+++ backend/apps/foods/models/product.py
@@ -3,6 +3,7 @@
 from django.db import models
 
 from apps.libs.utils import round_no_trailing_zeros
+from config.settings import CARB_KCAL_GRAM, FAT_KCAL_GRAM, PROTEIN_KCAL_GRAM
 
 from .food import Food
 
@@ -36,3 +37,34 @@ class FoodProduct(Food):
             res = f"{self.brand} {res}"
 
         return res
+
+    @property
+    def energy_meets_macros(self) -> bool:
+        """Check if energy meets macros.
+
+        Returns:
+            bool: True if energy meets macros, False otherwise.
+        """
+        return (
+            self.energy_kcal
+            == self.protein_g * PROTEIN_KCAL_GRAM
+            + self.fat_g * FAT_KCAL_GRAM
+            + self.carbs_g * CARB_KCAL_GRAM
+        )
+
+    def check_products(self):
+        if self.energy_meets_macros:
+            print(f">>>> {self}")
+        else:
+            macros_energy = (
+                self.protein_g * PROTEIN_KCAL_GRAM
+                + self.fat_g * FAT_KCAL_GRAM
+                + self.carbs_g * CARB_KCAL_GRAM
+            )
+            perc = "---"
+            if macros_energy > 0:
+                perc = round(self.energy_kcal / macros_energy * 100, 1)
+            print(
+                f"**** {self.energy_kcal - macros_energy}\t{perc} {self} - "
+                f"{self.energy_kcal} - {macros_energy}"
+            )
