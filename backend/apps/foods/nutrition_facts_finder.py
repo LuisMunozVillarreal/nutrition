778aea386eac66012d32481c11b8afac937df856
Luis Muñoz <luis.munoz.villarreal@gmail.com>
2025-07-28 20:12:05 +0100

    Scrape whole HTML page

--- backend/apps/foods/nutrition_facts_finder.py
+++ backend/apps/foods/nutrition_facts_finder.py
@@ -13,6 +13,8 @@ from django.conf import settings
 def get_product_nutritional_info_from_url(url: str) -> Dict[str, Any | float]:
     """Get product nutritional information from URL.
 
+    Use https://aistudio.google.com/ to tweak the prompt.
+
     Args:
         url (str): URL to scrape the date from.
 
@@ -25,9 +27,10 @@ def get_product_nutritional_info_from_url(url: str) -> Dict[str, Any | float]:
     model = genai.GenerativeModel(
         model_name="gemini-1.5-flash",
         system_instruction="""
-            You will receive an HTML code.
-            That HTML is of an Ocado product.
-            Extract information from that HTML.
+            You will receive an HTML page.
+            This page might contain javascript.
+            That page contains information of a food product.
+            Extract information from that page.
             Return only code, no text or markdown language.
             Return that information as a python dictionary.
             Your answer can't contain "```python".
@@ -52,13 +55,23 @@ def get_product_nutritional_info_from_url(url: str) -> Dict[str, Any | float]:
     )
 
     # Scrape
-    page = requests.get(url, timeout=60)
+    page = requests.get(
+        url,
+        timeout=60,
+        headers={
+            "User-Agent": (
+                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+                "(KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"
+            ),
+        },
+    )
     soup = BeautifulSoup(page.content, "html.parser")
-    body = soup.find("body")
+    html = soup.find("html")
+    print(html)
 
     # Analyse
     chat_session = model.start_chat()
-    response = chat_session.send_message(str(body))
+    response = chat_session.send_message(str(html))
 
     return ast.literal_eval(response.text)
 
