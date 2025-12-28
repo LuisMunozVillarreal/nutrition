f51e0713e75d230a38c47eb80db4772761a7e118
Luis Muñoz <luis.munoz.villarreal@gmail.com>
2025-12-28 19:18:49 +0000

    feat: add admin favicon and server helper

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,11 +1,17 @@
 """nutrition URL Configuration."""
 
 import django_sql_dashboard
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("dashboard/", include(django_sql_dashboard.urls)),
     path("_nested_admin/", include("nested_admin.urls")),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=settings.STATIC_URL + "favicon.ico"),
+    ),
 ]
