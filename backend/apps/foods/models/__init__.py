52c80e596e4cc4bb7d1201182a0901932b785a53
Luis Munoz Villarreal <luis.munoz.villarreal@gmail.com>
2026-08-10 23:34:42 +0100

    feat(foods): persist OFF cache and quota state

--- backend/apps/foods/models/__init__.py
+++ backend/apps/foods/models/__init__.py
@@ -3,6 +3,7 @@
 # flake8: noqa: F401
 from .cupboard import CupboardItem, CupboardItemConsumption
 from .food import Food
+from .open_food_facts import OpenFoodFactsCacheEntry, OpenFoodFactsRateLimit
 from .product import FoodProduct
 from .recipe import Recipe, RecipeIngredient
 from .serving import Serving
